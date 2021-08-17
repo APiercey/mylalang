e7bcbc4f3b07dbae06c5744ab20163ff2aa0fe7c
Alexander Piercey <alexander.piercey@gmail.com>
2021-08-17 22:17:34 +0200

    Divide native functions

--- src/native/mod.rs
+++ src/native/mod.rs
@@ -1,10 +1,20 @@
 use crate::core::env::Env;
 use crate::evaluate as root_evaluate;
 
-const FUNCTIONS: &str = include_str!("functions.my");
-const FILES: &str = include_str!("files.my");
+const ARITHMETIC_OPERATORS: &str = include_str!("arithmetic_operators.my");
+const COMPARISON_OPERATORS: &str = include_str!("comparison_operators.my");
+const CONSTRUCTOR_OPERATORS: &str = include_str!("constructor_operators.my");
+const FILE_FUNCTIONS: &str = include_str!("file_functions.my");
+const HIGHER_ORDER_FUNCTIONS: &str = include_str!("higher_order_functions.my");
+const MATH_FUNCTIONS: &str = include_str!("math_functions.my");
+const SEARCH_FUNCTIONS: &str = include_str!("search_functions.my");
 
 pub fn load(env: &Env) {
-    root_evaluate(&env, FUNCTIONS);
-    root_evaluate(&env, FILES);
+    root_evaluate(&env, ARITHMETIC_OPERATORS);
+    root_evaluate(&env, COMPARISON_OPERATORS);
+    root_evaluate(&env, CONSTRUCTOR_OPERATORS);
+    root_evaluate(&env, FILE_FUNCTIONS);
+    root_evaluate(&env, HIGHER_ORDER_FUNCTIONS);
+    root_evaluate(&env, MATH_FUNCTIONS);
+    root_evaluate(&env, SEARCH_FUNCTIONS);
 }
